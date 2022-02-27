ac336468cbb2b613f2b368f8bdf88b8531cae4a5
Dan <dan@dankinsley.com>
2022-02-27 11:14:55 -0500

    Update menu items to navigate withRouter HOC modified:   package.json modified:   src/App.js modified:   src/components/menu-item/menu-item.component.jsx modified:   src/components/menu-item/menu-item.styles.scss modified:   src/directory/directory.component.jsx modified:   src/index.js modified:   yarn.lock

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl}) => {
-  return (
-    <div className='menu-item'>
-      <div 
-        style={{backgroundImage: `url(${imageUrl})`}}
-        className='background-image'
-      />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+  <div
+    className={`menu-item ${size ? size : ''}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div 
+      style={{backgroundImage: `url(${imageUrl})`}}
+      className='background-image'
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-  );
-}
+  </div>
+);
 
-export default MenuItem;
+export default withRouter(MenuItem);
