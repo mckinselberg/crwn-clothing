6d151a34694ba076e463c2e4db07d91b36029af9
Dan <dan@dankinsley.com>
2022-03-13 16:16:31 -0400

    Updated app to redirect to home if user is signed in. modified:   src/App.js modified:   src/components/header/header.component.jsx modified:   src/firebase/firebase.utils.js modified:   src/pages/homepage/homepage.styles.scss modified:   src/redux/user/user.actions.js modified:   src/redux/user/user.reducer.jsx new file:   src/redux/user/user.types.js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,18 +15,21 @@ const Header = ({ currentUser }) => (
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/contact'>CONTACT</Link>
       {
-        currentUser ?
-        <div className="option" onClick={ () => auth.signOut() }>SIGN OUT</div>
-        :
-        <Link className="option" to='/signin'>SIGN IN</Link>
+        currentUser ? (
+          <div className="option" onClick={ () => auth.signOut() }>SIGN OUT</div>
+        ) : (
+          <Link className="option" to='/signin'>SIGN IN</Link>
+        )
       }
     </div>
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-});
+const mapStateToProps = state => {
+  return ({
+    currentUser: state.user.currentUser
+  })
+}
 
 
 export default connect(mapStateToProps)(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -34,7 +34,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log('error creating user', error.message);
     }
   }
-  
+
   return userRef;
 }
 
