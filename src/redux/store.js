29adea6412b9850f029bb1b786f637bc157a6134
Dan <dan@dankinsley.com>
2022-03-14 08:15:40 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,9 @@ import rootReducer from "./root-reducer";
 
 const middleWares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+// const store = createStore(rootReducer, applyMiddleware(...middleWares));
+// Redux Dev Tools: 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(...middleWares)));
 
 export default store;
